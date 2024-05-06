3c3a1aa552b776251423ec5cc74e713049cd9a1d
Nevo Biton <bitonnevo@gmail.com>
2024-05-06 12:15:12 +0300

    last exc7 sort

--- exc7/sort1.js
+++ exc7/sort1.js
@@ -1,25 +1,62 @@
 
 
-function sortArray(array) {
-    let tempdArr = array.slice();
-    let sortedArray = [];
+// function sortArray(array) {
+//     let tempdArr = array
+//     let sortedArray = [];
 
-    while (tempdArr.length > 0) {
-        let minIndex = 0;
-        let minValue = tempdArr[0];
+//     while (tempdArr.length > 0) {
+//         let minIndex = 0;
+//         let minValue = tempdArr[0];
 
-        for (let i = 1; i < tempdArr.length; i++) {
-            if (tempdArr[i] < minValue) {
-                minValue = tempdArr[i];
-                minIndex = i;
-            }
+//         for (let i = 1; i < tempdArr.length; i++) {
+//             if (tempdArr[i] < minValue) {
+//                 minValue = tempdArr[i];
+//                 minIndex = i;
+//             }
+//         }
+
+//         sortedArray.push(minValue);
+//         tempdArr.splice(minIndex, 1);
+//     }
+
+//     return sortedArray;
+// }
+
+let numArray = [7, 6, 5, 4, 3, 2];
+
+function computeMin(numbers, indexToStart) {
+
+    let minNumIndex = null
+    let minNumber = Number.MAX_SAFE_INTEGER
+    for (let i = indexToStart; i < numbers.length; i++) {
+        const currentNumber = numbers[i];
+        if (currentNumber < minNumber) {
+            minNumIndex = i
+            minNumber = currentNumber
         }
-
-        sortedArray.push(minValue);
-        tempdArr.splice(minIndex, 1);
     }
-
-    return sortedArray;
+    return minNumIndex
 }
 
-let numArray = [2, 4, 1, 9, 6, 3, 2];
+function switchNumbers(numbers, indexI, indexJ) {
+
+    let temp = numbers[indexI]
+    numbers[indexI] = numbers[indexJ]
+    numbers[indexJ] = temp
+}
+
+function sortNumbers(numbers) {
+
+    for (let i = 0; i < numbers.length; i++) {
+        const indexMin = computeMin(numbers, i);
+        switchNumbers(numbers, i, indexMin);
+    }
+}
+
+
+sortNumbers(numArray)
+console.log(numArray);
+
+
+
+
